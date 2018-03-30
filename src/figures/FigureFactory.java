18104dbed0fb449ba8772e41195222bac8e0554b
misterion <Ilyalas@gmail.com>
2018-03-31 00:11:19 +0300

    Changed switch to reflection

--- src/figures/FigureFactory.java
+++ src/figures/FigureFactory.java
@@ -2,25 +2,31 @@ package figures;
 
 import javafx.scene.canvas.GraphicsContext;
 import java.awt.geom.Point2D;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class FigureFactory {
 
-    public Figure getFigure(String figureType, GraphicsContext gc, Point2D.Double fPoint, Point2D.Double sPoint){
-        if (figureType == null){
+    public Figure getFigure(String figureType, GraphicsContext gc, Point2D.Double fPoint, Point2D.Double sPoint) {
+
+        figureType = "figures." + figureType;
+
+        Constructor c;
+        try {
+            c = Class.forName(figureType).getConstructor(GraphicsContext.class, Point2D.Double.class,Point2D.Double.class);
+        } catch (NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
             return null;
         }
-        switch (figureType) {
-            case "Line":
-                return new Line(gc, fPoint, sPoint);
-            case "Rectangle":
-                return new Rectangle(gc, fPoint, sPoint);
-            case "Oval":
-                return new Oval(gc, fPoint, sPoint);
-            case "Triangle":
-                return new Triangle(gc, fPoint, sPoint);
-            case "Star":
-                return new Star(gc, fPoint, sPoint);
+
+        Figure figure;
+        try {
+            figure = (Figure) c.newInstance(gc, fPoint, sPoint);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            return null;
         }
-        return null;
+
+        return figure;
     }
 }
